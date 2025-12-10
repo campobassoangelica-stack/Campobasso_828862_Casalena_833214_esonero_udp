8027606917255c2b8e2714f4f3c33512ba266f22
campobassoangelica-stack <campobassoangelica@gmail.com>
2025-12-10 18:35:53 +0100

    Allineato codice Eclipse con GitHub: aggiornati main.c e protocol.h

--- client-project/src/main.c
+++ client-project/src/main.c
@@ -1,5 +1,4 @@
 /*
-<<<<<<< HEAD
  * client.c
  *
  * UDP Client - Esonero Reti di Calcolatori
@@ -18,30 +17,10 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-=======
- * main.c
- *
- * UDP Client - Template for Computer Networks assignment
- *
- * This file contains the boilerplate code for a UDP client
- * portable across Windows, Linux, and macOS.
- */
-
-#if defined WIN32
-#include <winsock.h>
-#else
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <netinet/in.h>
->>>>>>> e2c86cd1c160ab08f8d5804cb372292b1bb5a4a6
 #include <netdb.h>
 #define closesocket close
 #endif
 
-<<<<<<< HEAD
 #include "protocol.h"
 
 // Funzione di gestione errori
@@ -265,47 +244,3 @@ int deserialize_response(weather_response_t *res, char *buffer) {
     clearwinsock();
     return 0;
 }
-=======
-#include <stdio.h>
-#include <stdlib.h>
-#include "protocol.h"
-
-#define NO_ERROR 0
-
-void clearwinsock() {
-#if defined WIN32
-	WSACleanup();
-#endif
-}
-
-int main(int argc, char *argv[]) {
-
-	// TODO: Implement client logic
-
-#if defined WIN32
-	// Initialize Winsock
-	WSADATA wsa_data;
-	int result = WSAStartup(MAKEWORD(2,2), &wsa_data);
-	if (result != NO_ERROR) {
-		printf("Error at WSAStartup()\n");
-		return 0;
-	}
-#endif
-
-	int my_socket;
-
-	// TODO: Create UDP socket
-
-	// TODO: Configure server address
-
-	// TODO: Implement UDP communication logic
-
-	// TODO: Close socket
-	// closesocket(my_socket);
-
-	printf("Client terminated.\n");
-
-	clearwinsock();
-	return 0;
-} // main end
->>>>>>> e2c86cd1c160ab08f8d5804cb372292b1bb5a4a6
--- server-project/src/protocol.h
+++ server-project/src/protocol.h
@@ -4,35 +4,54 @@
  * Shared header file for UDP client and server
  * Contains protocol definitions, data structures, constants and function prototypes
  */
-
 #ifndef PROTOCOL_H_
 #define PROTOCOL_H_
 
 #include <stdint.h>
 
-/*
- * ============================================================================
- * PROTOCOL CONSTANTS
- * ============================================================================
- */
+// Costanti condivise
+#define SERVER_PORT 56700
+#define BUFFER_SIZE 512
+#define DEFAULT_IP "127.0.0.1"
 
-// #define ...
+// Tipi di dati meteo
+#define TYPE_TEMPERATURE 't'
+#define TYPE_HUMIDITY    'h'
+#define TYPE_WIND        'w'
+#define TYPE_PRESSURE    'p'
 
-/*
- * ============================================================================
- * PROTOCOL DATA STRUCTURES
- * ============================================================================
- */
+// Codici di stato
+#define STATUS_SUCCESS           0
+#define STATUS_CITY_UNAVAILABLE  1
+#define STATUS_INVALID_REQUEST   2
 
-// Weather request and response structures 
+// Struttura richiesta client → server
+typedef struct {
+    char type;
+    char city[64];
+} weather_request_t;
 
-/*
- * ============================================================================
- * FUNCTION PROTOTYPES
- * ============================================================================
- */
+// Struttura risposta server → client
+typedef struct {
+    unsigned int status;
+    char type;
+    float value;
+} weather_response_t;
 
-// Add here the signatures of the functions you implement
+// Prototipi di funzioni comuni
+void errorhandler(char *errorMessage);
+void clearwinsock(void);
 
+// Prototipi funzioni di generazione dati
+float get_temperature(void);
+float get_humidity(void);
+float get_wind(void);
+float get_pressure(void);
+
+// Prototipi funzioni di serializzazione
+int serialize_request(weather_request_t *req, char *buffer);
+int deserialize_request(weather_request_t *req, char *buffer);
+int serialize_response(weather_response_t *res, char *buffer);
+int deserialize_response(weather_response_t *res, char *buffer);
 
 #endif /* PROTOCOL_H_ */
